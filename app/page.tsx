251bd0088b9ecd003d00cf8932cf6dcb099c282d
riabov2 <yan.riabov@gmail.com>
2026-09-19 07:40:47 +0300

    Use focused Pro Max homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,5 @@
-import Hero from "@/components/Hero";import LandingSections from "@/components/LandingSections";
-export default function Home(){return <><Hero/><LandingSections/></>}
+import ProMaxHome from "@/components/ProMaxHome";
+
+export default function Home() {
+  return <ProMaxHome />;
+}
